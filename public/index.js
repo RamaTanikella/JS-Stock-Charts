55153e363fc125d8500a645d91fd69121d1e2bc3
RamaTanikella <88115546+RamaTanikella@users.noreply.github.com>
2021-11-25 21:25:02 -0500

    stock charts solution

--- public/index.js
+++ public/index.js
@@ -1,9 +1,66 @@
+function getColor(stock){
+    if(stock === "GME"){
+        return 'rgba(61, 161, 61, 0.7)'
+    }
+    if(stock === "MSFT"){
+        return 'rgba(209, 4, 25, 0.7)'
+    }
+    if(stock === "DIS"){
+        return 'rgba(18, 4, 209, 0.7)'
+    }
+    if(stock === "BNTX"){
+        return 'rgba(166, 43, 158, 0.7)'
+    }
+}
+
+
+function getMaxs(stocks){
+    let r = stocks.map((stock) => {
+        return Math.max(...stock.values.map((value) => (value.high)))
+    })
+    return r
+    // [200,10,20,30]
+}
 async function main() {
 
     const timeChartCanvas = document.querySelector('#time-chart');
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
 
+    let response = await fetch("https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&apikey=a1342a723ef54ac295849a8fbe975c30&interval=1day")
+    let result = await response.json()
+    const { GME, MSFT, DIS, BNTX } = mockData;
+    const stocks = [GME, MSFT, DIS, BNTX];
+
+    new Chart(timeChartCanvas.getContext('2d'), {
+        type: 'line',
+        data: {
+            labels: stocks[0].values.map(value => value.datetime),
+            datasets: stocks.map( stock => ({
+                label: stock.meta.symbol,
+                data: stock.values.map(value => parseFloat(value.high)),
+                backgroundColor:  getColor(stock.meta.symbol),
+                borderColor: getColor(stock.meta.symbol),
+            }))
+        }
+    });
+
+    new Chart(highestPriceChartCanvas.getContext('2d'), {
+        type: 'bar',
+        data: {
+            labels: stocks.map(stock => stock.meta.symbol),
+            datasets: [
+              {
+                label: "Highest Price",
+                backgroundColor: stocks.map(stock => getColor(stock.meta.symbol)),
+                data: getMaxs(stocks)
+              }
+            ]
+        },
+       
+    });
 }
+
+
 
 main()
